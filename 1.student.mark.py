59fc10d8aca0e92e886427cf75bcb30b7300d19f
Okdat <datpt.22bi13081@usth.edu.vn>
2024-01-04 15:58:30 +0700

    create function to add students, students per class, course info. though with some inaccuracy

---
+++ 1.student.mark.py
@@ -0,0 +1,18 @@
+def add_students():
+    student_id = input("Enter student id: ")
+    name = input("Enter student name: ")
+    dob = input("Enter date of Birth: ")
+    students[student_id] = {'name' : name, 'dob' : dob, 'courses': {}}  
+    
+def studentNumperclass():
+    stdnum = input("How many students are there in a class? ")
+    
+def num_of_course():
+    courseNum = input("Enter number of course: ")
+    
+def course_info():
+    courseID =("Enter course ID: ")
+    courseName = ("Enter course name:")
+
+
+    
